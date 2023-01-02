9a8a25959f3abddd084c24903ef3e790b0bbc72c
p.lyahov <p.lyahov@a1qa.com>
2023-01-03 01:25:56 +0300

    Add dynamic page

--- src/main/java/com/pavel/englishhelper/EnglishHelperApplication.java
+++ src/main/java/com/pavel/englishhelper/EnglishHelperApplication.java
@@ -2,10 +2,8 @@ package com.pavel.englishhelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RequestMapping(value="hello")
 public class EnglishHelperApplication {
 
 	public static void main(String[] args) {
